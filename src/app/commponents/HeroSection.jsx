c7b2c3b999d1cc4ed6da2360f9d479aeb83dfa6c
danblock97 <generalgoldiez@gmail.com>
2024-03-16 09:28:53 +0000

    Hero component

---
+++ src/app/commponents/HeroSection.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
+
+const HeroSection = () => {
+	return (
+		<section>
+			<div className="grid grid-cols-1 sm:grid-cols-12">
+				<div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+						<span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-violet-600">
+							Welcome to AstroStats{" "}
+						</span>
+						<br />
+						<TypeAnimation
+							sequence={[
+								"Horoscopes",
+								1000,
+								"Apex Legends",
+								1000,
+								"League of Legends",
+								1000,
+								"Fortnite",
+								1000,
+							]}
+							wrapper="span"
+							speed={50}
+							repeat={Infinity}
+						/>
+					</h1>
+					<p className="text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl">
+						Your ultimate companion for staying updated on daily horoscopes and
+						tracking player stats across various popular games! Whether you're
+						seeking cosmic guidance or aiming to dominate in your favorite
+						gaming titles like Apex Legends, League of Legends, and Fortnite,
+						this bot has you covered.
+					</p>
+					<Link href={"#commands"}>
+						<button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-blue-600 to-violet-600 hover:bg-slate-200 text-white">
+							Check out my commands!
+						</button>
+					</Link>
+					<Link href={"#contact"}>
+						<button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-blue-600 to-violet-600 hover:bg-slate-800 text-white mt-3">
+							<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+								Contact
+							</span>
+						</button>
+					</Link>
+				</div>
+				<div className="col-span-4 place-self-center mt-4 lg:mt-0">
+					<div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
+						<Image
+							src="/images/astrostats.png"
+							alt="hero image"
+							className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-transparent"
+							width={300}
+							height={300}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default HeroSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from "./commponents/Navbar";
+import HeroSection from "./commponents/HeroSection";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col bg-[#121212]">
 			<Navbar />
+			<div className="container mt-24 mx-auto py-4 px-12">
+				<HeroSection />
+			</div>
 		</main>
 	);
 }
