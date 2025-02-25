292d0c4a8b4f9d297520a30019a2077d46a59368
Dan <generalgoldiez@gmail.com>
2025-02-25 07:33:35 +0000

    OPS-4 fix(Navbar): Implement delegated event binding for bug reporting button and update rendering logic

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -6,7 +6,14 @@ const MenuOverlay = ({ links }) => {
 		<ul className="flex flex-col py-4 items-center">
 			{links.map((link, index) => (
 				<li key={index}>
-					<NavLink href={link.path} title={link.title} />
+					{link.id ? (
+						// Render the "Report a Bug" button with the matching id for mobile.
+						<button id={link.id} className="block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded hover:text-white">
+							{link.title}
+						</button>
+					) : (
+						<NavLink href={link.path} title={link.title} />
+					)}
 				</li>
 			))}
 		</ul>
