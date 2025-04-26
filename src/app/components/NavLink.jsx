62cd7f4ef4b973734fa9f1efcab6360b7797b921
danblock97 <generalgoldiez@gmail.com>
2025-04-26 09:23:38 +0100

    feat(Navbar): Add id prop to NavLink and implement Jira issue collectors for bug reporting and feature requests

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,10 +1,11 @@
 const { default: Link } = require("next/link");
 
-const NavLink = ({ href, title }) => {
+const NavLink = ({ href, title, id }) => {
 	return (
 		<Link
 			href={href}
 			className="block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded md:p-0 hover:text-white"
+			id={id} // Add id prop here
 		>
 			{title}
 		</Link>
