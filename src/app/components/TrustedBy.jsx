f3fb261a386073129d1f4bdc258410d8d7272387
Dan <generalgoldiez@gmail.com>
2025-08-24 17:12:02 +0100

    feat(trusted-by): add TrustedBy component to showcase user trust metrics

---
+++ src/app/components/TrustedBy.jsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+const TrustedBy = () => {
+	return (
+		<div className="py-8">
+			<div className="container mx-auto px-4">
+				<div className="text-center">
+					<p className="text-sm text-gray-400 mb-4">
+						Trusted by over 500 servers and over 5000 Discord users
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TrustedBy;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection";
 import FeatruresHighlight from "./components/FeaturesHighlight";
+import TrustedBy from "./components/TrustedBy";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <div className="container mt-24 mx-auto py-4 px-12 flex-grow">
+        <TrustedBy />
         <HeroSection />
         <div className="mt-16">
           <FeatruresHighlight />
