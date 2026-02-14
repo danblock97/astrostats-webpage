1caa9407ca16d3210495fe17e383d7c39ed2f672
Dan <generalgoldiez@gmail.com>
2026-02-14 16:49:06 +0000

    Update trusted server count

--- src/app/components/TrustedBy.jsx
+++ src/app/components/TrustedBy.jsx
@@ -7,7 +7,7 @@ const TrustedBy = () => {
 			<div className="container mx-auto px-4">
 				<div className="text-center">
 					<p className="text-sm text-gray-400 mb-4">
-						Trusted by over 500 servers and over 55,000 Discord users
+						Trusted by over 600 servers and over 55,000 Discord users
 					</p>
 				</div>
 			</div>
